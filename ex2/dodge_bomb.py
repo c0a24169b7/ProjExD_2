946728e104e849f52f5b80f14030f49d6bf861bb
kanata_nakazato <c62_3@icloud.com>
2025-11-25 16:19:40 +0900

    ゲームオーバー画面の作成

---
+++ ex2/dodge_bomb.py
@@ -0,0 +1,120 @@
+import os
+import random
+import sys
+import time
+
+import pygame as pg
+from pygame import Surface
+
+WIDTH, HEIGHT = 1100, 650
+DELTA = {
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0),
+}
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+def gameover(screen: pg.Surface) -> None:
+    """ゲームオーバー画面を表示する関数"""
+    # --- 半透明の黒背景 ---
+    dark = pg.Surface((WIDTH, HEIGHT))
+    dark.set_alpha(150)  # ←透明度（0〜255）
+    dark.fill((0, 0, 0))
+    screen.blit(dark, (0, 0))
+
+    kk_img = pg.image.load("fig/8.png")
+
+    # --- GAME OVER テキスト ---
+    font = pg.font.Font(None, 150)
+    txt = font.render("GAME OVER", True, (255, 255, 255))
+    rect = txt.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+    screen.blit(txt, rect)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 200, 325
+    screen.blit(kk_img, kk_rct)
+    kk_rct2 = kk_img.get_rect()
+    kk_rct2.center = 900, 325
+    screen.blit(kk_img, kk_rct2)
+    pg.display.update()
+    time.sleep(5)
+
+
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    引数：こうかとんRectまたは爆弾Rect
+    戻り値：判定結果タプル（横方向，縦方向）
+    画面内ならTrue，画面外ならFalse
+    """
+    yoko, tate = True, True
+    if rct.left < 0 or WIDTH < rct.right:  # 横方向のはみ出しチェック
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom:  # 縦方向のはみ出しチェック
+        tate = False
+    return yoko, tate
+
+
+def main():
+    pg.display.set_caption("逃げろ！こうかとん")
+    screen = pg.display.set_mode((WIDTH, HEIGHT))
+    bg_img = pg.image.load("fig/pg_bg.jpg")
+    kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 300, 200
+    bb_img = pg.Surface((20, 20))  # 空のSurface
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 半径10の赤い円を描画
+    bb_img.set_colorkey((0, 0, 0))  # 黒色を透過色に設定
+    bb_rct = bb_img.get_rect()  # 爆弾Rect
+    bb_rct.centerx = random.randint(0, WIDTH)  # 爆弾横座標
+    bb_rct.centery = random.randint(0, HEIGHT)  # 爆弾縦座標
+    vx, vy = +5, +5  # 爆弾の横速度，縦速度
+    clock = pg.time.Clock()
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
+
+        if kk_rct.colliderect(bb_rct): # こうかとんと爆弾が衝突したら
+            gameover(screen)
+            print("ゲームオーバー")
+            time.sleep(5)
+            return
+
+        screen.blit(bg_img, [0, 0])
+
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0, 0]
+        # if key_lst[pg.K_UP]:
+        #     sum_mv[1] -= 5
+        # if key_lst[pg.K_DOWN]:
+        #     sum_mv[1] += 5
+        # if key_lst[pg.K_LEFT]:
+        #     sum_mv[0] -= 5
+        # if key_lst[pg.K_RIGHT]:
+        #     sum_mv[0] += 5
+        for key, mv in DELTA.items():
+            if key_lst[key]:
+                sum_mv[0] += mv[0]  # 横方向の移動量
+                sum_mv[1] += mv[1]  # 縦方向の移動量
+        kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):  # 画面外なら
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])  # 移動をなかったことにする
+        screen.blit(kk_img, kk_rct)
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:  # 横方向にはみ出ていたら
+            vx *= -1
+        if not tate:  # 縦方向にはみ出ていたら
+            vy *= -1
+        bb_rct.move_ip(vx, vy)
+        screen.blit(bb_img, bb_rct)
+        pg.display.update()
+        tmr += 1
+        clock.tick(50)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
